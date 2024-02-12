3dd5835916cfa1bd797274b5644b607b5f32bced
Abdulazizzn <104701146+Abdulazizzn@users.noreply.github.com>
2024-02-13 01:42:15 +0300

    hi abod
    Co-authored-by: Yaman  <YamanAlo@users.noreply.github.com>
    Co-authored-by: moony <moonyal6@users.noreply.github.com>

--- src/pages/Dashboard/Fekrah/index.tsx
+++ src/pages/Dashboard/Fekrah/index.tsx
@@ -118,19 +118,17 @@ export default function FekrahPage() {
       text: `Certainly! War is a state of armed conflict between different nations or groups. It is a complex and multifaceted event that can have wide-ranging impacts on societies, economies, and political systems.
 
         Wars can be fought for a variety of reasons, including territorial disputes, ideological differences, or the desire for power and resources. The causes of war are often rooted in long-standing conflicts or grievances between nations or groups.
-        
-        During a war, military forces engage in combat, utilizing a range of weapons, strategies, and tactics to gain an advantage over the enemy. The outcome of a war is determined by a combination of factors, including the strength and capability of the opposing forces, available resources, and the support of the civilian population.
-        
-        Wars have significant social, economic, and political consequences. They result in the loss of human lives, destruction of infrastructure, displacement of populations, and the disruption of economies. The impact of war can be felt for generations, as societies struggle to rebuild and recover from the devastation.
-        
-        War also has a profound effect on societies, leading to changes in political systems, cultural norms, and international relations. It can reshape geopolitical boundaries, alter power dynamics, and influence global affairs.
-        
-        Throughout history, there have been numerous wars that have had a profound impact on the world. Some notable examples include World War I, World War II, the Cold War, and ongoing conflicts in the Middle East. These wars have shaped the course of history, leaving a lasting impact on the lives of millions of people.
-        
-        Efforts have been made to prevent and mitigate the effects of war. International organizations, such as the United Nations, aim to promote peace, resolve conflicts diplomatically, and provide humanitarian aid to war-affected areas. However, war continues to be a reality in many parts of the world, underscoring the complex nature of human conflict and the challenges of maintaining peace.
         `,
     },
-  ]);
+    { id: 3, isRes: false, text: "Write about war" },
+    {
+      id: 4,
+      isRes: true,
+      text: `Certainly! War is a state of armed conflict between different nations or groups. It is a complex and multifaceted event that can have wide-ranging impacts on societies, economies, and political systems.
+
+        Wars can be fought for a variety of reasons, Certainly! War is a state of armed conflict between different nations or groups. It is a complex and multifaceted event l disputes, ideological differences, or the desire for power and resources. The causes of war are often rooted in long-standing conflicts or grievances between nations or groups.
+        `,
+    },]);
   const [isLoading, setIsLoading] = useState(false);
   const messageContainerRef = useRef<HTMLDivElement>(null);
 
@@ -169,6 +167,7 @@ export default function FekrahPage() {
   const chatView = chat.map((message: MessageProps) => (
             <ChatMessage aiResponse={message.isRes}>{message.text}</ChatMessage>
           ))
+
   const textfield = <PromptInput
           isLoading={isLoading}
           onValueChange={(value) => setPrompt(value)}
@@ -177,11 +176,11 @@ export default function FekrahPage() {
         />
 
   return (
-    <div className="flex flex-row h-full w-full overflow-hidden ">
-      <div className=" flex flex-col h-full max-w-full flex-1 overflow-auto">
-        <div className="flex-col gap-6 overflow-y-auto">
-          {chatView}
+    <div className=" flex flex-col justify-between h-screen max-w-full flex-1">
+        <div className="overflow-y-auto ">
+              {chatView}
         </div>
+      <div className=" bottom-8 w-full pb-12 px-10">
         {textfield}
       </div>
     </div>
