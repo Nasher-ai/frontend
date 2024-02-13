f8dd6f2bab42fa730297a109fd816132e296fc2f
m.moony.almo <m.moony.almo.6@gmail.com>
2024-02-13 21:56:41 +0300

    removed unused imports

--- src/pages/pagesData.tsx
+++ src/pages/pagesData.tsx
@@ -1,12 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { routerType } from "../types/router.types";
-import CheckEmailPage from "./Auth/Check Email";
-import ForgotPasswordPage from "./Auth/Forgot Password";
-import LoginPage from "./Auth/Login";
-import ResetPasswordPage from "./Auth/Reset Password";
-import SignUpPage from "./Auth/Sign Up";
-import SuccessResetPage from "./Auth/Success Reset";
-import VerifyPage from "./Auth/Verify";
+// import CheckEmailPage from "./Auth/Check Email";
+// import ForgotPasswordPage from "./Auth/Forgot Password";
+// import LoginPage from "./Auth/Login";
+// import ResetPasswordPage from "./Auth/Reset Password";
+// import SignUpPage from "./Auth/Sign Up";
+// import SuccessResetPage from "./Auth/Success Reset";
+// import VerifyPage from "./Auth/Verify";
 import FekrahPage from "./Dashboard/Fekrah";
 
 const pagesData: routerType[] = [
