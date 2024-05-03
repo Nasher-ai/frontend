7307e581a02b8e3b383480bad9242d44f763d2fb
m.moony.almo <m.moony.almo.6@gmail.com>
2024-05-04 00:32:20 +0300

    removed controls from whiteboard

--- src/pages/Dashboard/Hujra/Whiteboard/index.tsx
+++ src/pages/Dashboard/Hujra/Whiteboard/index.tsx
@@ -42,12 +42,13 @@ export default function App() {
       <ReactFlow
         nodes={nodes}
         edges={edges}
+        
         proOptions={proOptions}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
       >
-        <Controls position="bottom-right" className="bg-neutral-700" />
+        
         <Background variant={BackgroundVariant.Dots} gap={20} size={4} color="#161616" />
       </ReactFlow>
       <div className="absolute left-[1.5rem] top-1/2 transform -translate-y-1/2">
